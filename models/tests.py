891b681106831d944f9e8cb5bad9f3fe05fed346
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2024-01-24 15:38:29 +0200

    remove qg cc param

--- models/tests.py
+++ models/tests.py
@@ -327,7 +327,7 @@ class SecurityTestsSAST(db_tools.AbstractBaseMixin, db.Base, rpc_tools.RpcMixin)
                    f"-e galloper_url={vault_client.unsecret('{{secret.galloper_url}}')} " \
                    f"-e token=\"{vault_client.unsecret('{{secret.auth_token}}')}\" " \
                    f"{control_tower} " \
-                   f"-tid {self.test_uid} -qg true"
+                   f"-tid {self.test_uid}"
 
         if output == "cc":
             channel = self.scan_location
